2adfa1180931a1232cb7623f98c4cfc2e272f22f
Troy M. LaPolice <55710427+TroyLaPolice@users.noreply.github.com>
2022-07-05 16:31:40 -0400

    Update plotting_pipeline.R

--- plotting_pipeline.R
+++ plotting_pipeline.R
@@ -1,13 +1,22 @@
+# Loab Libraries
 library(ggplot2)
 library(tidyr)
 
-setwd("/home/tml5905/Documents/HuberLab/HunterGatherFarmerInteractions")
-input_file_extention = "_test"
-output_file_extention = "_test"
+# Set WD and input and output file name additions
 
+setwd("/home/tml5905/Documents/HuberLab/HunterGatherFarmerInteractions")
+
+input_file_extention = "_test"  # Should be the same as the ending of your SLiM output files
+                                #   if endings are default this will be an empty string.
+
+output_file_extention = "_test" # Addition to attach to the output files
+                                #   Best practice is to keep these the same for simplicity
+
+# Create file names
 square_input_name = paste("sim_square_wave_stats_per_year", input_file_extention, ".txt", sep = "")
 general_input_name = paste("sim_pop_stats_per_year", input_file_extention, ".txt", sep = "")
 
+# Check if files exist and read in those that do
 if ((file.exists(square_input_name)) && (file.exists(general_input_name))){
   square_input_file = read.csv(square_input_name, sep = " ", header = TRUE)
   general_input_file = read.csv(general_input_name, sep = " ", header = TRUE)
@@ -19,7 +28,12 @@ if ((file.exists(square_input_name)) && (file.exists(general_input_name))){
   print("Requested files do not exist")
 }
 
+# If the model was run on a square, plot the data
 if (file.exists(square_input_name)){
+  
+  # Plot ratios
+  
+  # Push ratio data to its own data frame
   ratio_dat = data.frame(square_input_file$Year,
                          square_input_file$RatioFarmerToHG, square_input_file$RatioFarmerToHG_Quad1,
                          square_input_file$RatioFarmerToHG_Quad2, square_input_file$RatioFarmerToHG_Quad3, 
@@ -28,24 +42,31 @@ if (file.exists(square_input_name)){
                          square_input_file$RatioFarmerToHG_Quad8, square_input_file$RatioFarmerToHG_Quad9, 
                          square_input_file$RatioFarmerToHG_Quad10)
   
+  
+  # Name columns
   colnames(ratio_dat) <- c("Year", "Total_RatioFarmerToHG", "RatioFarmerToHG_Quad1", "RatioFarmerToHG_Quad2", 
                            "RatioFarmerToHG_Quad3", "RatioFarmerToHG_Quad4", "RatioFarmerToHG_Quad5", 
                            "RatioFarmerToHG_Quad6", "RatioFarmerToHG_Quad7", "RatioFarmerToHG_Quad8", 
                            "RatioFarmerToHG_Quad9", "RatioFarmerToHG_QuadTen")
   
+  # Remove non-informative ratio data
   head_ratio_dat = head(ratio_dat, -7200)
   
+  # Convert to tidy format
   ratio_dat_tidyr = tidyr::pivot_longer(head_ratio_dat, cols=c("Total_RatioFarmerToHG", "RatioFarmerToHG_Quad1", "RatioFarmerToHG_Quad2", 
                                                                "RatioFarmerToHG_Quad3", "RatioFarmerToHG_Quad4", "RatioFarmerToHG_Quad5", 
                                                                "RatioFarmerToHG_Quad6", "RatioFarmerToHG_Quad7", "RatioFarmerToHG_Quad8", 
                                                                "RatioFarmerToHG_Quad9", "RatioFarmerToHG_QuadTen"),
                                         names_to='variable', values_to="value")
   
+  # Plot
   ratio_plot = ggplot(ratio_dat_tidyr, aes(x=Year, y=value, fill=variable)) +
     geom_bar(stat='identity', position='dodge') +  geom_col(width = 1)
   
+  # Create output filename
   plot_out = paste("ratio_plot", output_file_extention, ".tiff", sep = "")
   
+  # Save ratio plot
   ggsave(plot_out, plot = last_plot(), units = "in", width = 10, height = 5, device='tiff', dpi=700)
   
 }
