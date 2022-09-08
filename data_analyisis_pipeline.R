fcd94807c9fe86e39e1a5b346ebcf7bf764b5c4e
Troy M. LaPolice <55710427+TroyLaPolice@users.noreply.github.com>
2022-09-08 12:45:04 -0400

    Update data_analyisis_pipeline.R

--- data_analyisis_pipeline.R
+++ data_analyisis_pipeline.R
@@ -1,5 +1,10 @@
 # Load Libraries
 library(data.table)
+library(ggplot2)
+
+# ----------------------------------------------------------------------------------------------------------------
+# Set inputs and read in files to be used for analysis
+# ----------------------------------------------------------------------------------------------------------------
 
 # Set input params
 setwd("/home/tml5905/Documents/HunterGatherFarmerInteractions/cluster_runs/8-4")
@@ -14,8 +19,9 @@ square_files = lapply(square_file_names, read.csv)
 param_file = lapply(param_file_name, read.csv)
 
 # ----------------------------------------------------------------------------------------------------------------
-
 # Transforming data to "long" format and combining different parameter combinations
+# ----------------------------------------------------------------------------------------------------------------
+
 all_sim_data = lapply(1:length(square_files), function(x) 
 {
   params = strsplit(as.character(param_file[[1]][x,]), split = "   ")[[1]]
@@ -41,69 +47,59 @@ all_sim_data = lapply(1:length(square_files), function(x)
 all_sim_data = rbindlist(all_sim_data)
 
 # ----------------------------------------------------------------------------------------------------------------
-
 # Transforming data to include km values for partitions
+# ----------------------------------------------------------------------------------------------------------------
 
-# Add extra column with partition numbers to be replaced with km values
-all_sim_data_km_values = cbind(all_sim_data, all_sim_data$Partition)
-names(all_sim_data_km_values)[names(all_sim_data_km_values) == "V2"] = "Mid_Point_km"
-
+# Calculate the number of partitions used
 num_parts = max(as.integer(all_sim_data$Partition))
 
 # Calculate where the first partition midpoint point is
-first_partition_midpoint = ((map_size_km / num_parts) / 2)
+partition_size = (map_size_km / num_parts)
 
-#Set this to be the previous midpoint before calculating further in the loop
-prev_midpt = first_partition_midpoint
-
-# Initialize list of midpoints starting with first partition
-midpoints = first_partition_midpoint
-
-# Loop through partitions appending midpoints
-for (partition in 1:(num_parts-1)) {
-  partition_midpoint = first_partition_midpoint * 2 + prev_midpt
-  midpoints = append(midpoints, partition_midpoint)
-  prev_midpt = partition_midpoint
-}
-
-# Convert entries in DF to character from factor for replacement
-all_sim_data_km_values$Mid_Point_km = as.character(all_sim_data_km_values$Mid_Point_km)
-
-# Replace partition numbers with corresponding km values for midpoint
-for (partition in 1:(num_parts)) {
-  all_sim_data_km_values$Mid_Point_km[all_sim_data_km_values$Mid_Point_km == partition] = midpoints[partition]
-}
-
-# Turn back to factor
-all_sim_data_km_values$Mid_Point_km = as.factor(all_sim_data_km_values$Mid_Point_km)
+all_sim_data[, Mid_Point_km := as.numeric(Partition)*partition_size - partition_size/2]
 
 # ----------------------------------------------------------------------------------------------------------------
-
-test_year_df = all_sim_data_km_values[Year == 600 & 
-                                        Scale_Factor == "Scale Factor = 0.5" & 
-                                        Movement_SD == "Movement SD = 10km" & 
-                                        Learning_Prob == "Learning Prob = 0.0" & 
-                                        Assortative_Mating == "Assortative Mating = None"]
-
-test_year_df_simple = data.table(as.character(test_year_df$Mid_Point_km), as.character(test_year_df$RatioFarmerToHG_Partition))
-colnames(test_year_df_simple) = c("km", "ratio")
-
+# Function for approximating distance where 50% of the population is farmer
 # ----------------------------------------------------------------------------------------------------------------
 
-interpolate = function(data){
+interpolate = function(km, ratio){
   
-  # Approximate a distance from the x-axis in km when the ratio of farmers to HGs is 50/50
-  interpolation = approx(test_year_df_simple$ratio, test_year_df_simple$km, xout=0.5, method="linear")
-
+  if (all(ratio < 0.5) | all(ratio > 0.5)) return(NA)
+  
+  # Approximate a distance on the x-axis in km when the ratio of farmers to HGs is 50/50
+  interpolation = approx(ratio, km, xout=0.5, method="linear")
+  
   # Pull out the distance from the approximation list
   approximated_x = interpolation[[2]]
-
-  plot(test_year_df_simple, pch = 19)
-  points(approximated_x, 0.5, col='red', pch=19)
   
   # Return value
   return(approximated_x)
   
 }
 
-interpolate(test_year_df_simple)
+# ----------------------------------------------------------------------------------------------------------------
+# Run on all data and create new columns for distance and calculate speed
+# ----------------------------------------------------------------------------------------------------------------
+
+# Add column to data for the distance traveled on the x-axis in km when the ratio of farmers to HGs is 50/50
+all_sim_data[, km_50perc := as.numeric(0)]
+all_sim_data[, km_50perc := interpolate(km = Mid_Point_km, ratio = RatioFarmerToHG_Partition), 
+                       .(Year, Scale_Factor, Movement_SD, Learning_Prob, Assortative_Mating)]
+
+# Run a linear model to calculate the speed of wave and add as a column
+all_sim_data[, speedOfWave := lm(km_50perc ~ Year)$coeff[2], .(Scale_Factor, Movement_SD, Learning_Prob, Assortative_Mating)]
+
+# ----------------------------------------------------------------------------------------------------------------
+# Plot to make visual checks
+# ----------------------------------------------------------------------------------------------------------------
+
+ggplot(all_sim_data[Learning_Prob == "Learning Prob = 0.0" & Assortative_Mating == "Assortative Mating = None"]) + 
+  geom_point(aes(Year, km_50perc, col= Movement_SD)) + facet_grid(Scale_Factor ~ .)
+
+ggplot(all_sim_data[Learning_Prob == "Learning Prob = 0.0" & Assortative_Mating == "Assortative Mating = None" & Year %% 200 == 0]) + 
+  geom_line(aes(Mid_Point_km, RatioFarmerToHG_Partition, col= factor(Year))) + facet_grid(Scale_Factor ~ Movement_SD)
+
+ggplot(all_sim_data) + 
+  geom_point(aes(Movement_SD, speedOfWave, col= Learning_Prob)) + facet_grid(Scale_Factor ~ Assortative_Mating)
+
+
